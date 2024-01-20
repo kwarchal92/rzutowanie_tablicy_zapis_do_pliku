bfe4385bdec485fbbb36167e1acce09ccb473363
kwarchal <kamilwarchal295@gmail.com>
2024-01-20 10:52:11 +0100

    obiekt - czytajdanezPliku + poprawki

--- src/zapis_tablicy_do_pliku/Main.java
+++ src/zapis_tablicy_do_pliku/Main.java
@@ -7,12 +7,12 @@ class matrix
 {
     int rozmiar = 10;
 
-    public void czytajDane()
+    public void czytajDane(int tablica[][], int rozmiar)
     {
         int i, j;
 
-        System.out.println("Tworzymy tablice 10x10");
-        for (i = 0; i < j; i++)
+        System.out.println("\nTworzymy tablice 10x10 \n");
+        for (i = 0; i < rozmiar; i++)
         {
             for (j = 0; j < rozmiar; j++)
             {
@@ -32,9 +32,9 @@ class matrix
         int i, j;
 
         System.out.println();
-        System.out.println("Zapisujemy tablice 10x10 do pliku");
+        System.out.println("Zapisujemy tablice 10x10 do pliku \n");
 
-        FileWriter fr = new FileWriter(dane.txt);
+        FileWriter fw = new FileWriter("dane.txt");
 
         for (i =0; i <rozmiar; i++)
         {
@@ -46,12 +46,48 @@ class matrix
 
             System.out.println();
         }//i
+
+        fw.close(); //zamykamy plik
     }
 
-    ///////////////////////////////////////////////////////////////////////////////
-}
+    public void czytajdanezPliku(int tablica[][], int rozmiar)
+            throws IOException
+    {
+        int i, j;
+        System.out.println();
+        System.out.println("Odczytujemy tablice 10x10 z pliku. \n");
+
+        FileReader fr = new FileReader("dane.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        for(i = 0; i < rozmiar; i++)
+        {
+            for (j = 0; j < rozmiar; j++)
+            {
+                tablica[i][j] = (int) br.read(); //odczytujemy tablice z pliku i rzutujemy
+                System.out.print(tablica[i][j] + " ");
+            }//j
+            System.out.println();
+        }//i
+        fr.close();//zamykamy plik
+    }
+} // koniec klasy matrix
 
 
+public class Main
+{
+    public static void main(String[] args)
+        throws IOException
+    {
+        int rozmiar = 10;
 
-public class Main {
+        int tab[][] = new int[rozmiar][rozmiar];
+        int tab1[][] = new int[rozmiar][rozmiar];
+
+        matrix matrix1 = new matrix(); //deklarujemy i tworzymy obiekt matrix1
+
+        matrix1.czytajDane(tab, rozmiar);
+        matrix1.zapiszdanedoPliku(tab, rozmiar);
+        matrix1.czytajdanezPliku(tab1, rozmiar);
+    }
 }
